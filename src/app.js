bf650e0923dff4a3355b948ee6deea766b13117c
redius01 <uju1346@gmail.com>
2024-10-09 19:09:06 +0900

    프런트 단 javascript  파일 만들기

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@
 
 //모듈
 const express = require("express");
+const path = require('path');
 const app = express();
+
 
 
 
@@ -15,6 +17,7 @@ app.set("views", "./views")
 app.set("view engine", "ejs")
 
 
+app.use(express.static(`${__dirname}/src/public`));
 app.use("/", home); //use -> 미들웨어를 등록해주는 메서드.
 
 module.exports = app;
