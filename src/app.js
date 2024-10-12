d5fcde1f3b92c2148e0814328d168ebce591866e
redius01 <uju1346@gmail.com>
2024-10-13 08:14:13 +0900

    node mon 적용

--- src/app.js
+++ src/app.js
@@ -1,24 +1,31 @@
-"use strict";
+'use strict';
 
-//모듈
-const express = require("express");
-const path = require('path');
+// 모듈
+const express = require('express');
+const path = require('path'); // path 모듈 추가
 const app = express();
 
 
+//코파일럿
+//app.use(express.static(path.join(__dirname, 'src/public')));
 
+//커서
+app.use(express.static(path.join(__dirname, 'public')));
 
-//라우팅
+//app.use(express.static(`${__dirname}/src/public`));
+//app.use(express.static(`{__dirname}/src/public`));
+
+// 라우팅
 const home = require("./routes/home");
 
+// 앱 세팅
+//app.set("views", "src/views");
+app.set("views", path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
 
-//앱세팅
-app.set("views", "./views")
-app.set("view engine", "ejs")
+app.use("/", home);   // use: 미들웨어를 등록해주는 메소드
 
-
-app.use(express.static(`${__dirname}/src/public`));
-app.use("/", home); //use -> 미들웨어를 등록해주는 메서드.
 
 module.exports = app;
+
 
