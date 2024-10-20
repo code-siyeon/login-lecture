487139ed22136642799ef93d805e82f765426208
redius01 <uju1346@gmail.com>
2024-10-21 00:01:09 +0900

    회원가입 라우팅 구현

--- src/public/js/home/register.js
+++ src/public/js/home/register.js
@@ -12,30 +12,33 @@ console.log("hello regi")
 
 
 function register() {
-    const req = {
-        id: id.value,
-        name: name.value,
-        psword: psword.value,
-        confirmPsword: confirmPsword.value,
-    };
-    console.log(req);
-    fetch("/register", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(req),
-    })
-        .then((res) => res.json())
-        .then((res) => {
-            if (res.success) {
-                location.href = "/login";
-            } else {
-                alert(res.msg);
-            }
-        })
-        .catch((error) => {
-            console.error("회원가입 중 에러 발생:", error);
-        });
+    if (!id.value) return alert("아이디를 입력해주시오");
+    if (psword.value !== confirmPsword.value) return alert("비밀번호가 일치하지 않소");
 }
+
+
+const req = {
+    id: id.value,
+    name: name.value,
+    psword: psword.value,
+};
+console.log(req);
+fetch("/register", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+})
+    .then((res) => res.json())
+    .then((res) => {
+        if (res.success) {
+            location.href = "/login";
+        } else {
+            alert(res.msg);
+        }
+    })
+    .catch((error) => {
+        console.error("회원가입 중 에러 발생:", error);
+    });
 
