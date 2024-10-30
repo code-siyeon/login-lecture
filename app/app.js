6a7c5d147304a9de538730c359b7df963a76c2d5
redius01 <uju1346@gmail.com>
2024-10-30 22:21:01 +0900

    백엔드 맛보기 최종 최적화

--- app/app.js
+++ app/app.js
@@ -8,11 +8,8 @@ dotenv.config();
 
 const app = express();
 
-
 // 라우팅
 const home = require("./src/routes/home");
-
-
 
 // 앱 세팅
 app.set("views", "./src/views");
@@ -20,12 +17,10 @@ app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 
-
 // URL 을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", home);   // use -> 미들웨어를 등록해주는 메소드
-
 
 app.use((req, res, next) => {
     console.log('요청된 URL:', req.url);
